298e62c326340510b612df10c42bd3f07c03dd82
victor <victor.fa@students.salle.url.edu>
2026-01-03 17:57:01 +0100

    Added client supabase

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Cliente de Supabase para uso en el lado del cliente
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
